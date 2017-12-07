fe36131b28256e4b5e040c7f74fc9654cb981bda
TimiH <hestim@gmail.com>
2017-12-07 13:04:10 +0100

    Part2 for day 4

--- day4.py
+++ day4.py
@@ -12,24 +12,62 @@
 #     aa bb cc dd aaa is valid - aa and aaa count as different words.
 #
 # The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
-
+#----------------------------------------------------------
+# --- Part Two ---
+#
+# For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+#
+# For example:
+#
+#     abcde fghij is a valid passphrase.
+#     abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+#     a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+#     iiii oiii ooii oooi oooo is valid.
+#     oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+#
+# Under this new system policy, how many passphrases are valid?
 
 def Main():
-    correctPhrases = 0
+    #counter for Part1 and Part2
+    correctPhrasesPart1 = 0
+    correctPhrasesPart2 = 0
+
     #openfile
     data = open("day4_passphrases.txt", "r")
     for line in data:
-        if isPassphraseValid(line):
-            correctPhrases +=1
+        if isWordIn(line):
+            correctPhrasesPart1 +=1
+        if isAnagrammIn(line):
+            correctPhrasesPart2 +=1
 
-    print(correctPhrases)
+    print("Answer to Part1: " + str(correctPhrasesPart1))
+    print("Answer to Part2: " + str(correctPhrasesPart2))
 
-def isPassphraseValid(phrase):
+
+def isWordIn(phrase):
     words = phrase.split()
     valid = True
     while words:
         test = words.pop()
         if test in words:
+            valid = False
+
+    return valid;
+
+def isAnagrammIn(phrase):
+    words = phrase.split()
+    valid =True
+    wordSortet = []
+
+    #Create new list that sorts each word alphabetically
+    for word in words:
+        new = ''.join(sorted(word))
+        wordSortet.append(new)
+
+    #test if popped word is in the list
+    while wordSortet:
+        word = wordSortet.pop()
+        if word in wordSortet:
             valid = False
 
     return valid;
